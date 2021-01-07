870b96ebf01d45bdfdd3fb9ad852e9080aca4e78
klmpkaiman <77088892+klmpkaiman@users.noreply.github.com>
2021-01-07 13:49:03 +0700

    Create property.php

---
+++ property.php
@@ -0,0 +1,9 @@
+<?php
+//cara penulisan class dan property oop php - @pulung_coding
+class hewan {            //claas hewan
+  var $jeniskelamin;    //property jenis kelamin
+  var $warna;            //property warna
+  var $nama;             //property nama
+  
+}
+?>
