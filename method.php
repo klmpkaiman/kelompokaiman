dffea447217a30664a5baf94740cee753ca93202
klmpkaiman <77088892+klmpkaiman@users.noreply.github.com>
2021-01-07 13:50:13 +0700

    Create method.php

---
+++ method.php
@@ -0,0 +1,20 @@
+<?php
+//cara penulisan class dan property oop php - @pulung_koding
+class hewan{
+     //property oop
+     var $jeniskelamin;
+     var $warna;
+     var $nama;
+     
+     //mothod oop
+     function maju (){
+     //isi method
+       
+     
+}
+     function berhenti(){
+       //isi method
+     }
+ }
+ 
+ ?>    
